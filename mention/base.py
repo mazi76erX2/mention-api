3bf34612392d4ad416cc808288dec34c4fd9b44a
Xolani Mazibuko <mazi76erx@gmail.com>
2018-11-04 00:12:49 +0200

    Updated Create mention api method

--- mention/base.py
+++ mention/base.py
@@ -66,7 +66,6 @@ class AppDataAPI(Mention):
         data = response.json()
 
         return data
-    
 
 
 class FetchAnAlertAPI(Mention):
@@ -78,10 +77,10 @@ class FetchAnAlertAPI(Mention):
             Mention API access_token
 
         account_id: string
-            Id of the account.
+            ID of the account.
 
         alert_id: string
-            Id of the alert.
+            ID of the alert.
         """
         self.access_token = access_token
         self.account_id = account_id
@@ -116,6 +115,149 @@ class FetchAnAlertAPI(Mention):
                 pass
             data = response.json()
 
+        return data    
+
+
+class CreateAnAlertAPI(Mention):
+    def __init__(self,
+                 access_token,
+                 account_id,
+                 alert_id,
+                 name,
+                 query,
+                 languages,
+                 countries=None,
+                 sources=None,
+                 blocked_sites=None,
+                 noise_detection=None,
+                 reviews_pages=None):
+        """
+        Parameters
+        ----------
+        access_token: string
+            Mention API access_token
+
+        account_id: string
+            ID of the account.
+
+        alert_id: string
+            ID of the alert.
+
+        name: string
+            Alert name.
+
+        query: dict
+            Query is a dictionary that can be of two different types: basic or
+            advanced.
+            
+            eg.
+            query = {
+                'type'='basic',
+                'included_keywords' : ["NASA", "Arianespace", "SpaceX", "Pockocmoc"],
+                'required_keywords' : ["mars"],
+                'excluded_keywords' : ["nose", "fil d'ariane"],
+                'monitored_website' : ["domain":"www.nasa.gov", "block_self":true]
+            }
+
+            OR
+
+            query = {
+                'type' : 'advanced',
+                'query_string' : '(NASA AND Discovery) OR (Arianespace AND Ariane)'
+            }
+            
+        languages: list [str]
+            A list of language codes. eg: ['en']
+
+        countries: list [str]
+            A list of country codes. eg: ['US', 'RU', 'XX']
+
+        sources: list [str]
+            A list of sources from which mentions should be tracked.
+            Must be either web, twitter, blogs, forums, news, facebook, images or videos
+
+        blocked_sites: list [str] 
+            A list of blocked sites from which you don't want mentions to be tracked.
+
+        noise_detection: boolean
+            Enables noise detection.
+
+        reviews_pages: list [str]  
+            List of reviews pages.
+
+        """
+        self.access_token = access_token
+        self.account_id = account_id
+        self.alert_id = alert_id
+        self.name = name
+        self.query = query
+        self.languages = languages
+        self.countries = countries
+        self.sources = sources
+        self.blocked_sites = blocked_sites
+
+        if noise_detection is not None:
+            self.noise_detection = utils.transform_boolean(noise_detection)
+        else:
+            self.noise_detection = noise_detection
+        
+        self.reviews_pages = reviews_pages
+        super(FetchAnAlertAPI, self).__init__(access_token)
+
+
+    @property
+    def params(self):
+        params = {}
+        params["access_token"] = self.access_token
+        params["account_id"] = self.account_id
+        params["alert_id"] = self.alert_id
+        params["name"] = self.name
+        params["query"] = self.query
+        params["languages"] = self.languages
+        params["countries"] = self.countries if self.cursor else ""
+        params["sources"] = self.sources if self.sourceselse ""
+        params["blocked_sites"] = self.blocked_sites if self.blocked_sites else ""
+        params["noise_detection"] = self.noise_detection if noise_detection else ""
+        params["reviews_pages"] = self.reviews_pages if reviews_pages else ""
+
+        for key, value in list(params.items()):
+            if value == '':
+                del params[key]
+        
+        return params
+
+
+    @property
+    def url(self):
+        end_url= "/accounts/{account_id}/alerts/{alert_id}/mentions?"
+
+        # Returns copy of dictionary excluding certain keys
+        # https://stackoverflow.com/questions/31433989/
+        # return-copy-of-dictionary-excluding-specified-keys
+        def without_keys(d, keys):
+            return {x: d[x] for x in d if x not in keys}
+
+        keys = {"access_token", "account_id", "alert_id"}
+        parameters = without_keys(self.params, keys)
+
+        for key, value in list(parameters.items()):
+            if value != '':
+                end_url += '&' + key + '={' + key + '}'
+        
+        end_url = end_url.format(**self.params)
+        return self._base_url + end_url
+
+
+    def query(self):
+        with requests.Session() as session:
+            session.auth = OAuth2BearerToken(self.access_token)
+            response = session.post(self.url, data = {'key':'value'})
+            try:
+                response.raise_for_status()
+            except HTTPError:
+                pass
+            data = response.json()
+
         return data
     
 
@@ -129,7 +271,7 @@ class FetchAlertsAPI(Mention):
             Mention API access_token
 
         account_id: string
-            Id of the account.
+            ID of the account.
 
         """
         self.access_token = access_token
@@ -172,13 +314,13 @@ class FetchAMentionAPI(Mention):
             Mention API access_token
 
         account_id: string
-            Id of the account.
+            ID of the account.
 
         alert_id: string
-            Id of the alert.
+            ID of the alert.
 
         mention_id: string
-            Id of the mention.
+            ID of the mention.
         """
         self.access_token = access_token
         self.account_id = account_id
@@ -223,6 +365,7 @@ class FetchMentionsAPI(Mention):
                  access_token,
                  account_id,
                  alert_id,
+                 since_id=None,
                  limit=None,
                  before_date=None, # 2018-07-07T00:00:00.12345+02:00
                  not_before_date=None, # #2018-07-01T00:00:00.12345+02:00
@@ -245,16 +388,22 @@ class FetchMentionsAPI(Mention):
             Mention API access_token
 
         alert_id: string
-            Id of the alert.
+            ID of the alert.
+
+        since_id: string
+            Returns mentions ordered by id
+            Can not be combined with before_date, not_before_date, cursor.
 
         limit: string
             Number of mentions to return. max 1000.
 
         before_date: string
-            Mentions Before date in (yyyy-MM-ddTHH:mm:ss:SSSSS+HH:mm) format
+            Mentions Before date in 'yyyy-MM-dd HH:mm' format
+            eg. '2018-11-25 12:00'
 
         not_before_date: string
-            Mentions After date in (yyyy-MM-ddTHH:mm:ss:SSSSS+HH:mm) format
+            Mentions Not before date in 'yyyy-MM-dd HH:mm' format
+            eg. '2018-10-04 12:00'
 
         source: string
             Must be either web, twitter, blogs, forums, news, facebook, images or videos
@@ -302,12 +451,14 @@ class FetchMentionsAPI(Mention):
 
         self.limit = limit
 
+        self.since_id = since_id
+
         if before_date is not None:
             self.before_date = utils.transform_date(before_date)
         else:
             self.before_date = before_date
 
-        if before_date is not None:
+        if not_before_date is not None:
             self.not_before_date = utils.transform_date(not_before_date)
         else:
             self.not_before_date = not_before_date
@@ -352,23 +503,42 @@ class FetchMentionsAPI(Mention):
         params["access_token"] = self.access_token
         params["account_id"] = self.account_id
         params["alert_id"] = self.alert_id
-        
-        params["limit"] = self.limit
-        params["before_date"] = self.before_date if self.before_date else ""
-        params["not_before_date"] = self.not_before_date if self.not_before_date else ""
+
+        if self.since_id:
+            params["since_id"] = self.since_id
+        else:
+            params["before_date"] = self.before_date if self.before_date else ""
+            params["not_before_date"] = self.not_before_date if self.before_date else ""
+            params["cursor"] = self.cursor if self.cursor else ""
+
+        if self.unread:
+            params["unread"] = self.unread
+        else:
+            if (self.favorite) and
+            ((self.folder == "inbox") or (self.folder == "archive")):
+                params["favorite"] = self.favorite
+                params["folder"] = self.folder
+            else:
+                 params["folder"] = self.folder if self.folder else ""   
+            params["q"] = self.q if self.q else ""
+            params["tone"] = self.tone if self.tone else ""
+
+        if int(self.limit) > 1000:
+            params["limit"] = "1000"
+        elif int(self.limit) < 1:
+            params["limit"] = ""
+        else:
+            params["limit"] = self.limit
+
         params["source"] = self.source if self.source else ""
-        params["unread"] = self.unread if self.unread else ""
-        params["favorite"] = self.favorite if self.favorite else ""
-        params["folder"] = self.folder if self.folder else ""
-        params["tone"] = self.tone if self.tone else ""
+
         params["countries"] = self.countries if self.countries else ""
         params["include_children"] = self.include_children if self.include_children else ""
         params["sort"] = self.sort if self.sort else ""
         params["languages"] = self.languages if self.languages else ""
         params["timezone"] = self.timezone if self.timezone else ""
-        params["q"] = self.q if self.q else ""
-        params["cursor"] = self.cursor if self.cursor else ""
-
+        
+        #Deletes parameter if it does not have a value
         for key, value in list(params.items()):
             if value == '':
                 del params[key]
@@ -378,6 +548,99 @@ class FetchMentionsAPI(Mention):
     @property
     def url(self):
         end_url= "/accounts/{account_id}/alerts/{alert_id}/mentions?"
+
+        # Returns copy of dictionary excluding certain keys
+        def without_keys(d, keys):
+            return {x: d[x] for x in d if x not in keys}
+
+        keys = {"access_token", "account_id", "alert_id"}
+        parameters = without_keys(self.params, keys)
+
+        for key, value in list(parameters.items()):
+            if value != '':
+                end_url += '&' + key + '={' + key + '}'
+        
+        end_url = end_url.format(**self.params)
+        return self._base_url + end_url
+
+
+    def query(self):
+        with requests.Session() as session:
+            session.auth = OAuth2BearerToken(self.access_token)
+            response = session.get(self.url)
+            try:
+                response.raise_for_status()
+            except HTTPError:
+                pass
+            data = response.json()
+
+        return data
+
+
+class FetchMentionChildrenAPI(Mention):
+    """""This class will allow you to fetch a list of all children mentions for
+    a given mention.
+    """""
+    def __init__(self, access_token, account_id, alert_id, mention_id,
+                 limit=None, before_date=None):
+        """
+        Parameters
+        ----------
+        access_token: string
+            Mention API access_token
+
+        account_id: string
+            ID of the account.
+
+        alert_id: string
+            ID of the alert.
+
+        mention_id: string
+            ID of the mention.
+
+        limit: string
+            Number of mentions to return. max 1000.
+
+        before_date: string
+            Mentions Before date in 'yyyy-MM-dd HH:mm' format
+            eg. '2018-11-25 12:00'
+        """
+        self.access_token = access_token
+        self.account_id = account_id
+        self.alert_id = alert_id
+        self.mention_id = mention_id
+        self.limit = limit
+        
+        if before_date is not None:
+            self.before_date = utils.transform_date(before_date)
+        else:
+            self.before_date = before_date
+        super(FetchChildrenMentionAPI, self).__init__(access_token)
+
+
+    @property
+    def params(self):
+        params = {}
+        params["access_token"] = self.access_token
+        params["account_id"] = self.account_id
+        params["alert_id"] = self.alert_id
+        params["mention_id"] = self.mention_id
+        params["before_date"] = self.before_date if self.before_date else ""
+
+        if int(self.limit) > 1000:
+            params["limit"] = "1000"
+        elif int(self.limit) < 1:
+            params["limit"] = ""
+        else:
+            params["limit"] = self.limit
+        
+        return params
+
+
+    @property
+    def url(self):
+        end_url= ("/accounts/{account_id}/alerts/{alert_id}/mentions/"
+                  "{mention_id}/children?")
 
         def without_keys(d, keys):
             return {x: d[x] for x in d if x not in keys}
